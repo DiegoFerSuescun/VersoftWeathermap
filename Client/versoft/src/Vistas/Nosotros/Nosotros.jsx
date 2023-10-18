3c2c72a1d3907011b774954db93824f221d91d64
DiegoSuescun <dfsuesc@gmail.com>
2023-10-18 18:01:26 -0500

    Terminada vistas falta estilizar la principal y el nosotros con css pero ya renderiza todo lo que se desea

--- Client/versoft/src/Vistas/Nosotros/Nosotros.jsx
+++ Client/versoft/src/Vistas/Nosotros/Nosotros.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import imagen1 from "../../Multimedia/Versoft.webp"
+import { NavLink } from "react-router-dom";
 
 export default function Nosotros(props){
 
     return(
         <div>
-            <h1>nosotros</h1>
+            <img src={imagen1} alt="imagen" />
+            <h1>Desarrollador</h1>
+            <h4>Diego Fernando Suescun Gacheta</h4>
+            <h1>Objetivo</h1>
+            <h4>Esta pagina permite al usuario buscar su ciudad en una extensa api y de manera organizada proyecta el clima actual en la ciudad deseada, aparte da el reporte de la temperatura en tiempo real y la temperatura mas alta y mas baja esperada en el dia y por ultimo da el reporte del viento que se esta presentando en la ciudad, si no encuentra tu ciudad es intuitiva y te lo indica para que puedas realizar nuevamente la busqueda</h4>
+            <h1>Tecnologias</h1>
+            <h3>Para la creacion de esta pagina web se utilizaron herramientas tales como Javascript, Reactjs, Redux, React-Redux, Axios, Css, HTML, SWAL2, </h3>
+            <NavLink to='/principal'>
+            <button>Volver</button>
+            </NavLink>
+
         </div>
     )
 };
